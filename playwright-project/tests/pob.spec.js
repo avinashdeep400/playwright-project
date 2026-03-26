79199f04cbda485f98711cc73915ca8091c7cf24
Avinash Deep <polavarapu.avinashdeep@ferguson.com>
2026-03-26 15:33:25 +0530

    Env Variables

--- playwright-project/tests/pob.spec.js
+++ playwright-project/tests/pob.spec.js
@@ -3,13 +3,10 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://ferguson-tst.mdm.stibosystems.com/#');
   await page.getByRole('textbox', { name: 'someone@example.com' }).click();
-  await page.getByRole('textbox', { name: 'someone@example.com' }).fill('polavarapu.avinashdeep@ferguson.com');
+  await page.getByRole('textbox', { name: 'someone@example.com' }).fill('process.env.EMAIL');
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('textbox', { name: 'Enter the password for' }).click();
-  await page.getByRole('textbox', { name: 'Enter the password for' }).press('CapsLock');
-  await page.getByRole('textbox', { name: 'Enter the password for' }).fill('MDM');
-  await page.getByRole('textbox', { name: 'Enter the password for' }).press('CapsLock');
-  await page.getByRole('textbox', { name: 'Enter the password for' }).fill('MDMcareer@12');
+  await page.getByRole('textbox', { name: 'Enter the password for' }).press('process.env.PASSWORD');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.goto('https://login.microsoftonline.com/common/SAS/ProcessAuth');
   await page.getByRole('button', { name: 'No' }).click();
